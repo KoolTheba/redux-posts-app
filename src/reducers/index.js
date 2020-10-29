973fc378384317b2e6bd0bf7daf43f1a979a25f3
Theba Gomez <gomez.teba@gmail.com>
2020-10-29 21:06:36 +0100

    feat(): Combined reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,13 @@
 import { combineReducers } from 'redux'
-import postsReducer from './postsReducer'
 
-const reducer = combineReducers({
-    posts: postsReducer
+import postsReducer from './postsReducer'
+import postReducer from './postReducer'
+import commentsReducer from './commentsReducer'
+
+const rootReducer = combineReducers({
+  posts: postsReducer,
+  post: postReducer,
+  comments: commentsReducer,
 })
 
-export default reducer
+export default rootReducer
